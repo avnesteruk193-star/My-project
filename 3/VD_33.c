6052713806b7306bc6e7b504224d847b3c227b85
Rocket_9 <avnesteruk193@gmail.com>
2026-07-22 11:46:04 +0300

    done Úloha 3.3

---
+++ 3/VD_33.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main () {
+    int f , g;
+    scanf("%d %d", &f, &g);
+    printf("\n");
+    for (int i = f; i <= g; i++) {
+        if (i % 3 == 0) {
+            printf("%d ", i);
+        }
+    }
+    
+    return 0;
+}
